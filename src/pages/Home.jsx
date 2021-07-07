52c9087743c01d6c401f1eb48077d9a2c18413d9
beyond009 <1249413181@qq.com>
2021-07-07 22:36:59 +0800

    dfx 0.7.0

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import { Siderbar } from "../compoents/Sidebar"
+import { Widgets } from "../compoents/Widgets"
+import { Feed } from "../compoents/Feed"
+
+export default class Home extends Component {
+  render() {
+    return <div></div>
+  }
+}
