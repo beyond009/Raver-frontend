b78fcb0bec08af822f65516505a3406ba916d815
beyond009 <1249413181@qq.com>
2021-07-09 17:50:40 +0800

    big fixed

--- src/compoents/Feed.jsx
+++ src/compoents/Feed.jsx
@@ -7,26 +7,35 @@ import FlipMove from "react-flip-move";
 function Feed() {
   const [posts, setPosts] = useState([]);
 
-  // useEffect(() => {
-  //   db.collection("posts").onSnapshot((snapshot) =>
-  //     setPosts(snapshot.docs.map((doc) => doc.data()))
-  //   );
-  // }, []);
+  useEffect(() => {
+    // db.collection("posts").onSnapshot((snapshot) =>
+    //   setPosts(snapshot.docs.map((doc) => doc.data()))
+    // );
+    var post = {
+      text: "fuck you asshole",
+      displayName: "ddd009",
+      username: "ddd009",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1398625159016292358/uxVxE5Oc_400x400.jpg",
+      image:
+        "https://www.nme.com/wp-content/uploads/2020/07/GettyImages-1052372952.jpg",
+    };
+    // setPosts(post);
+  }, []);
 
   return (
     <div className="feed">
       <FlipMove>
-        {posts.map((post) => (
-          <Post
-            key={post.text}
-            displayName={post.displayName}
-            username={post.username}
-            verified={post.verified}
-            text={post.text}
-            avatar={post.avatar}
-            image={post.image}
-          />
-        ))}
+        {/* {posts.map((post) => ( */}
+        <Post
+          key="fxxk !"
+          displayName="ddd009"
+          username="ddd009"
+          text="fxxK !"
+          avatar="https://pbs.twimg.com/profile_images/1398625159016292358/uxVxE5Oc_400x400.jpg"
+          image="https://www.nme.com/wp-content/uploads/2020/07/GettyImages-1052372952.jpg"
+        />
+        {/* // ))} */}
       </FlipMove>
     </div>
   );
--- src/compoents/TweetBox.jsx
+++ src/compoents/TweetBox.jsx
@@ -28,7 +28,7 @@ function TweetBox() {
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
-          <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+          <Avatar src="https://pbs.twimg.com/profile_images/1398625159016292358/uxVxE5Oc_400x400.jpg" />
 
           <input
             onChange={(e) => setTweetMessage(e.target.value)}
