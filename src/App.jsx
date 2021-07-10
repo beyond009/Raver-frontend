ba14fc1be753fac98c55c92a70520ef87a1efa30
beyond009 <1249413181@qq.com>
2021-07-10 12:00:37 +0800

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -12,32 +12,52 @@ import { AuthClient } from "@dfinity/auth-client";
 import { BrowserRouter, Route, Switch, Link, Router } from "react-router-dom";
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      islogin: false,
-      identity: null,
-    };
-  }
-
-  onloginChange = (isl = false) => {
-    this.setState({ islogin: isl });
-  };
-  goToLoginPage = () =>
+const App = () => {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     islogin: false,
+  //     identity: null,
+  //   };
+  // }
+  const [isLogin, setIsLogin] = useState(false);
+  const [loading, setLoading] = useState(false);
+  // onloginChange = (isl = false) => {
+  //   this.setState({ islogin: isl });
+  // };
+  var authClient = null;
+  var identity = null;
+  const goToLoginPage = () =>
     history.push({
       pathname: "/login",
     });
-  async handleAuthenticated(authClient) {
-    this.setState({ islogin: true });
-    history.push({
-      pathname: "/home",
-    });
-    console.log("login in success!");
-    const identity = await authClient.getIdentity();
-    this.setState({ identity: identity });
+  // async handleAuthenticated(authClient) {
+  //   this.setState({ islogin: true });
+  //   history.push({
+  //     pathname: "/home",
+  //   });
+  //   console.log("login in success!");
+  //   const identity = await authClient.getIdentity();
+  //   this.setState({ identity: identity });
+  // }
+  async function handleAuthenticated(authClient) {
+    setIsLogin(true);
+    console.log("login success");
+    history.push({ pathname: "/home" });
+    identity = await authClient.getIdentity();
+    console.log(identity);
   }
-  async handleLogin() {
+  useEffect(async () => {
+    console.log("Check login!");
+    const authClient = await AuthClient.create();
+    if (await authClient.isAuthenticated()) {
+      handleAuthenticated(authClient);
+    } else {
+      console.log("go to login page!");
+      goToLoginPage();
+    }
+  });
+  const handleLogin = async () => {
     console.log("handle login");
     const authClient = await AuthClient.create();
     await authClient.login({
@@ -47,73 +67,44 @@ class App extends Component {
       identityProvider:
         "http://localhost:8080/?canisterId=rwlgt-iiaaa-aaaaa-aaaaa-cai",
     });
-  }
-  componentWillMount() {
-    // (async () => {
-    //   console.log("Check login!");
-    //   const authClient = await AuthClient.create();
-    //   if ((await authClient.isAuthenticated()) ) {
-    //     handleAuthenticated(authClient);
-    //     this.setState({ islogin: true });
-    //   }
-    //   if (!this.state.islogin) {
-    //     console.log("go to login page");
-    //     this.goToLoginPage();
-    //   }
-    // })();
-    // history.push({ pathname: "/home" });
-  }
+  };
 
-  render() {
-    return (
-      <div className="app">
-        {this.state.islogin ? <Sidebar /> : null}
-        {/* <Sidebar /> */}
-        <Router history={history}>
-          <Route
-            exact
-            path="/login"
-            component={(props) => {
-              let obj = Object.assign(
-                {},
-                { handleLogin: this.handleLogin },
-                props
-              );
-              return <Login {...obj} />;
-            }}
-          />
-          <Route
-            exact
-            path="/home"
-            component={(props) => {
-              let obj = Object.assign(
-                {},
-                { identity: this.state.identity },
-                props
-              );
-              return <Home {...obj} />;
-            }}
-          />
-          <Route
-            exact
-            path="/profile"
-            component={(props) => {
-              let obj = Object.assign(
-                {},
-                { identity: this.state.identity },
-                props
-              );
-              return <Login {...obj} />;
-            }}
-          />
-          <Route exact path="/signup" compoent={Profile} />
-          {/* <Route exact path="/" compoent={} /> */}
-        </Router>
-        {this.state.islogin ? <Widgets /> : null}
-        {/* <Widgets /> */}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="app">
+      {isLogin ? <Sidebar /> : null}
+      {/* <Sidebar /> */}
+      <Router history={history}>
+        <Route
+          exact
+          path="/login"
+          component={(props) => {
+            let obj = Object.assign({}, { handleLogin: handleLogin }, props);
+            return <Login {...obj} />;
+          }}
+        />
+        <Route
+          exact
+          path="/home"
+          component={(props) => {
+            let obj = Object.assign({}, { identity: identity }, props);
+            return <Home {...obj} />;
+          }}
+        />
+        <Route
+          exact
+          path="/profile"
+          component={(props) => {
+            let obj = Object.assign({}, { identity: identity }, props);
+            return <Login {...obj} />;
+          }}
+        />
+        <Route exact path="/signup" compoent={Profile} />
+        {/* <Route exact path="/" compoent={} /> */}
+      </Router>
+      {isLogin ? <Widgets /> : null}
+      {/* <Widgets /> */}
+    </div>
+  );
+};
 
 export default App;
