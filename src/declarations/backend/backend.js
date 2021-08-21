d540b9d0c2a06af387cf76cb9e96cda737a7d3b7
beyond009 <1249413181@qq.com>
2021-08-21 16:37:02 +0800

    fix style

--- src/declarations/backend/backend.js
+++
@@ -1,3 +0,0 @@
-import idlImport from './backend.did.js';
-export const idlFactory = idlImport;
-export const canisterId = "r7inp-6aaaa-aaaaa-aaabq-cai";
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,8 @@ import { Actor, HttpAgent } from "@dfinity/agent";
 import { AuthClient } from "@dfinity/auth-client";
 import { Button } from "@material-ui/core";
 import dfinityicon from "../../assets/dfinity.png";
-
+import avatar_1 from "../../assets/avatar-1.png";
+import avatar_2 from "../../assets/avatar-2.png";
 import { idlFactory, canisterId } from "dfx-generated/backend";
 import history from "../History";
 import "./Login.css";
@@ -16,15 +17,17 @@ export default class Login extends Component {
   }
   render() {
     return (
-      <div>
-        <Button
-          variant="contained"
-          onClick={this.props.handleLogin}
-          className="Login_LoginButton"
-        >
-          Login With &nbsp;&nbsp;{" "}
-          <img src={dfinityicon} style={{ height: "16px" }} />
-        </Button>
+      <div className="login">
+        <div className="login_position">
+          <Button
+            variant="contained"
+            onClick={this.props.handleLogin}
+            className="Login_LoginButton"
+          >
+            Login With &nbsp;&nbsp;{" "}
+            <img src={dfinityicon} style={{ height: "23px" }} />
+          </Button>
+        </div>
         {/* <Button
           variant="contained"
           onClick={this.toSignup}
